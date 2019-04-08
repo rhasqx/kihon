6fb8d96ff113c748fad59eccb5da1f9d6b9fb7b5
Matt <rhasqx@posteo.nz>
2019-04-08 17:45:28 +0200

    Update tts

--- app/models/token.rb
+++ app/models/token.rb
@@ -2,6 +2,9 @@ class Token < ApplicationRecord
     
     paginates_per 20
 
+    after_create :tts
+    after_update :tts
+
     def readonly?
         File.exists?(Rails.root.join('readonly'))
     end
@@ -14,4 +17,17 @@ class Token < ApplicationRecord
       end
     end
     
+    def tts
+      aiff = "/tmp/tts.aiff"
+      %w(hiragana katakana kanji).each do |component|
+        mp3 = Rails.root.join("public", "assets", "tts", component, "#{self.id}.mp3")
+        if !File.file?(mp3)
+          text = self.send(component) || ""
+          if !text.empty?
+              cmd = "say -v Kyoko \"#{text}\" -o \"#{aiff}\" && ffmpeg -i \"#{aiff}\" -y -f mp3 -acodec libmp3lame -ab 192000 -ar 44100 \"#{mp3}\""
+              status = `#{cmd}`
+          end
+        end
+      end
+    end
 end
